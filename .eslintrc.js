f47377720d2254f654a2be6378b51d5a1e44e4cd
exuanbo <exuanbo@protonmail.com>
2020-10-29 22:04:42 +0000

    feat: extend `@typescript-eslint/recommended`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,22 @@
-const prettierStandard = require('./.prettierrc.js')
-
 module.exports = {
-  extends: ['standard', 'prettier', 'prettier/standard'],
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/standard'
+  ],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    'prettier/prettier': ['error', prettierStandard]
-  }
+    'prettier/prettier': ['error', require('./.prettierrc.js')]
+  },
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ]
 }
