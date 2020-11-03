4b2463e33b5ad743302b554c0f41cb68297b794b
exuanbo <exuanbo@protonmail.com>
2020-11-03 23:29:12 +0000

    build: add esm and dts

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,30 @@
-const typescript = require('@rollup/plugin-typescript')
-const json = require('@rollup/plugin-json')
-const pkg = require('./package.json')
+import typescript from '@rollup/plugin-typescript'
+import json from '@rollup/plugin-json'
+import dts from 'rollup-plugin-dts'
+import pkg from './package.json'
 
-module.exports = {
-  input: 'src/index.ts',
-  plugins: [typescript(), json({ compact: true })],
-  external: ['path', 'standard-engine', 'eslint'],
-  output: {
-    file: pkg.main,
-    format: 'cjs'
+export default [
+  {
+    external: ['path', ...Object.keys(pkg.dependencies)],
+    input: 'src/index.ts',
+    plugins: [typescript(), json({ compact: true })],
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs'
+      },
+      {
+        file: pkg.module,
+        format: 'es'
+      }
+    ]
+  },
+  {
+    input: '.cache/src/index.d.ts',
+    output: {
+      file: pkg.types,
+      format: 'es'
+    },
+    plugins: [dts()]
   }
-}
+]
