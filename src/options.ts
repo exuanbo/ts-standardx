6915a3941a7372e64c754cd02c1dce929e191c18
exuanbo <exuanbo@protonmail.com>
2020-11-05 23:37:50 +0000

    refactor: use description as tagline

--- src/options.ts
+++ src/options.ts
@@ -1,14 +1,21 @@
 import path from 'path'
 import eslint from 'eslint'
 import { ProvidedOptions } from 'standard-engine-ts'
-import { name, version, bugs, homepage, dependencies } from '../package.json'
+import {
+  name,
+  version,
+  description,
+  bugs,
+  homepage,
+  dependencies
+} from '../package.json'
 
 const ENGINE = 'standard-engine-ts'
 
 export const opts: ProvidedOptions = {
   cmd: name,
   version: `${version} (${ENGINE} v${dependencies[ENGINE]})`,
-  tagline: 'TypeScript Standard Style',
+  tagline: description,
   homepage: homepage,
   bugs: bugs.url,
   eslint,
