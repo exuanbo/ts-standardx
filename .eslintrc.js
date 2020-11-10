3e49a884fcc77fa803bc42ce117920a1274653a9
exuanbo <exuanbo@protonmail.com>
2020-11-10 20:56:41 +0000

    feat: read `tsconfig.json` from root path

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,12 @@
+const { existsSync } = require('fs')
+const { join } = require('path')
+const rootPath = require('standard-engine-ts').getRootPath()
+
+const getTsconfig = () => {
+  const filePath = join(rootPath, 'tsconfig.json')
+  return (existsSync(filePath) && filePath) || undefined
+}
+
 module.exports = {
   extends: ['standard', 'plugin:prettier/recommended', 'prettier/standard'],
   parser: '@typescript-eslint/parser',
@@ -10,7 +19,7 @@ module.exports = {
       files: ['**/*.ts'],
       extends: ['standard-with-typescript', 'prettier/@typescript-eslint'],
       parserOptions: {
-        project: './tsconfig.json'
+        project: getTsconfig() ?? './tsconfig.json'
       }
     }
   ]
