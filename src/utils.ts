ebb8eadebd9a064d90cd34a6587e0e56a040dd93
exuanbo <exuanbo@protonmail.com>
2020-11-14 01:05:58 +0000

    refactor(utils): rm unnecessary variable
    replace with string

--- src/utils.ts
+++ src/utils.ts
@@ -17,9 +17,11 @@ export const getEslintrc = (): Record<string, unknown> | undefined => {
 
   if (eslintrcList.length > 0) {
     const eslintrc = require(eslintrcList[0])
-    const eslintrcStr = JSON.stringify(eslintrc)
     return JSON.parse(
-      eslintrcStr.replace(/require\('ts-standardx\/\.eslintrc\.js'\)/, '{}')
+      JSON.stringify(eslintrc).replace(
+        "require('ts-standardx/.eslintrc.js')",
+        '{}'
+      )
     )
   }
   return undefined
