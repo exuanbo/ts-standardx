6e74f5c94119949fe5502b2d2a1aa98902c62f77
exuanbo <exuanbo@protonmail.com>
2020-11-19 20:20:55 +0000

    refactor(utils): add inline comment

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import fs from 'fs'
 import path from 'path'
 import { getRootPath } from 'standard-engine-ts'
@@ -17,7 +16,9 @@ export const getEslintrc = (): Record<string, unknown> | undefined => {
     .filter(excludeUndefined)
 
   if (eslintrcList.length > 0) {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
     const eslintrc = require(eslintrcList[0])
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return JSON.parse(
       JSON.stringify(eslintrc).replace(
         "require('ts-standardx/.eslintrc.js')",
