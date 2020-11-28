30f723c9bad159a5c6144b2302f01c22feb4f9b8
exuanbo <exuanbo@protonmail.com>
2020-11-28 12:22:11 +0000

    refactor: rename variable

--- src/utils.ts
+++ src/utils.ts
@@ -9,17 +9,17 @@ const excludeUndefined = <T>(item: T | undefined): item is T =>
 export const getEslintrc = (): Linter.BaseConfig | undefined => {
   const rootPath = getRootPath()
 
-  const eslintrcList = ['.eslintrc.js', '.eslintrc.json', '.eslintrc']
+  const eslintrcArr = ['.eslintrc.js', '.eslintrc.json', '.eslintrc']
     .map(file => {
       const filePath = path.join(rootPath, file)
       return (fs.existsSync(filePath) && filePath) || undefined
     })
     .filter(excludeUndefined)
 
-  if (eslintrcList.length > 0) {
+  if (eslintrcArr.length > 0) {
     return JSON.parse(
       // eslint-disable-next-line @typescript-eslint/no-var-requires
-      JSON.stringify(require(eslintrcList[0])).replace(
+      JSON.stringify(require(eslintrcArr[0])).replace(
         "require('ts-standardx/.eslintrc.js')",
         '{}'
       )
