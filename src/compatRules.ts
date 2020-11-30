f5aff57bcadf923555953a695dd199c2ab5255e4
exuanbo <exuanbo@protonmail.com>
2020-11-30 20:16:57 +0000

    refactor(compatRules): add jsdoc comments

--- src/compatRules.ts
+++ src/compatRules.ts
@@ -4,9 +4,6 @@ import { rules } from 'eslint-config-standard/eslintrc.json'
 type RuleName = keyof typeof rules
 type RuleEntry = typeof rules[RuleName]
 
-/**
- * @link https://github.com/typescript-eslint/typescript-eslint/tree/master/packages/eslint-plugin
- */
 const RULES_MAP: RuleName[] = [
   'lines-between-class-members',
   'no-dupe-class-members',
@@ -20,15 +17,42 @@ const RULES_MAP: RuleName[] = [
 ]
 
 export const compatRules: Partial<Linter.RulesRecord> = {
-  'no-undef': 'off',
+  /**
+   * Use `@typescript-eslint/naming-convention` instead
+   * @link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/naming-convention.md
+   */
   camelcase: 'off',
 
+  /**
+   * Error when using the same option from `eslint-config-standard`
+   * @link https://github.com/standard/eslint-config-standard/blob/master/eslintrc.json#L58
+   */
   'dot-notation': 'off',
+  /**
+   * @link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/dot-notation.md
+   */
   '@typescript-eslint/dot-notation': 'error',
 
+  /**
+   * The original rule is turned off by `@typescript-eslint/eslint-recommended`
+   * @link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/src/configs/eslint-recommended.ts#L15
+   */
+  '@typescript-eslint/no-dupe-class-members': 'error',
+
+  /**
+   * Extend `import/no-duplicates` from eslint-config-standard
+   * @link https://github.com/standard/eslint-config-standard/blob/master/eslintrc.json#L229
+   */
   '@typescript-eslint/no-duplicate-imports': 'error',
 
+  /**
+   * Add @typescript-eslint specific options
+   * @link https://github.com/standard/eslint-config-standard/blob/master/eslintrc.json#L180
+   */
   'no-use-before-define': 'off',
+  /**
+   * @link https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md
+   */
   '@typescript-eslint/no-use-before-define': [
     'error',
     {
