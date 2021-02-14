bfabfe4b87eebc1774919e081a8d8376fc3fd5d7
exuanbo <exuanbo@protonmail.com>
2021-02-14 22:23:40 +0000

    feat: export class `CLI`

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import {
-  Linter as StandardLinter,
   ProvidedOptions,
+  Linter as StandardLinter,
+  CLI as StandardCLI,
   mergeConfig
 } from 'standard-engine-ts'
 import { options } from './options'
+
+export { options }
 
 export class Linter extends StandardLinter {
   constructor(customOptions?: ProvidedOptions) {
@@ -11,4 +14,8 @@ export class Linter extends StandardLinter {
   }
 }
 
-export { options }
+export class CLI extends StandardCLI {
+  constructor(customOptions?: ProvidedOptions) {
+    super(mergeConfig(options, customOptions))
+  }
+}
